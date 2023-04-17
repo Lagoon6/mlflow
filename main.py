ae9198c183bab4a6b7355e2e416a40d47b2f427a
Lagoon6 <arkanum3608@naver.com>
2023-04-17 20:50:18 +0900

    mlflow main.py 테스트 04/17

---
+++ main.py
@@ -0,0 +1,19 @@
+import mlflow
+
+from model import MlflowModel
+
+def start_mlflow(model, n):
+    mlflow.set_experiment('start')
+    
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        pass
+    except:
+        pass
+    
